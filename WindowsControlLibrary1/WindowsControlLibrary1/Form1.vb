82e69fa0dc5db85722554282733fb98fe3cb9425
ashumeow <ashumeow@live.com>
2020-02-27 09:27:03 +0300

    update

---
+++ WindowsControlLibrary1/WindowsControlLibrary1/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("hello! I am working.")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
+'
+' ashumeow
+'
